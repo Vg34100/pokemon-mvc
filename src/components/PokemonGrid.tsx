2f61f361b0ee3ff5cc20c1179779031849681472
Lalo <eroblestena@gmail.com>
2024-11-22 15:09:36 -0800

    style: homepage color change, font change, dex number added

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -6,23 +6,24 @@ import { Pokemon } from '@/models/Pokemon.model';
 export function PokemonGrid({ pokemon }: { pokemon: Pokemon[] }) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-      {pokemon.map((p) => (
+      {pokemon.map((p, index) => (
         <Link
           href={`/pokemon/${p.id}`}
           key={p.id}
-          className="border rounded-lg p-4 text-center hover:shadow-lg transition-shadow"
+          className=" relative bg-gray-200 text-pokedexBlack rounded-lg p-4 text-center hover:shadow-pokedexGray transition-shadow"
         >
-          <div className="relative w-24 h-24 mx-auto">
+          <p className="absolute top-1 left-2 text-sm text-gray-500">#{(index + 1).toString().padStart(4, "0")}</p>
+          <div className="relative w-full h-24 mx-auto">
             <Image
               src={p.sprite}
               alt={p.name}
               fill
               sizes="(max-width: 96px) 100vw, 96px"
-              priority={p.id <= 12}
+              priority={index <= 12}
               className="object-contain"
             />
           </div>
-          <h2 className="mt-2 capitalize font-medium">{p.name}</h2>
+          <h2 className="mt-2 capitalize font-pokemon">{p.name}</h2>
         </Link>
       ))}
     </div>
